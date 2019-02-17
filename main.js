ec3bd3235c383fecaf471a80ce26b0128d3cf0a3
JackyLiu97 <jackyliu977@gmail.com>
2019-02-17 02:57:07 -0500

    got destination to open on google maps

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 var map, infoWindow;
+var orig
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10.75,
     center: { lat: 40.7128, lng: -74.006 } // location of NYC
   });
 
-  // NOTE: This uses cross-domain XHR, and may not work on older browsers.
+  window.onload = function() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          orig = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+        
+        }, function(error) {
+            alert('Error occurred. Error code: ' + error.code);         
+        },{timeout:5000});
+    }else{
+        alert('no geolocation support');
+    }
+};
 
   fetch("https://s3.amazonaws.com/litter-basket/data.json")
     .then(response => {
@@ -13,30 +28,41 @@ function initMap() {
     })
     .then(data => {
       // Work with JSON data here
-      var orig;
+      /*
       navigator.geolocation.getCurrentPosition(function(position) {
         orig = {
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
       });
+      */
       for (let i = 0; i < data.features.length; i++) {
         var latlng = {
           lat: data.features[i].geometry.coordinates[1],
           lng: data.features[i].geometry.coordinates[0]
         };
-
         var marker = new google.maps.Marker({
           position: latlng,
           map: map,
-          title: "Litter Basket"
-        });
-
+          title: "Litter Basket",
+          url: ""
+        }) 
+        /*
+        let markerLink = "https://www.google.com/maps/dir/?api=1" + "&origin=" + orig[lat] + "," + orig[lng] +
+                            "&destination=" + latlng[lat] + "," + latlng[lng]
+        marker.url = markerLink
+        */
         var directionsService = new google.maps.DirectionsService();
         var directionsDisplay = new google.maps.DirectionsRenderer();
         google.maps.event.addListener(marker, "click", function() {
+          markerLink = "https://www.google.com/maps/dir/?api=1" + "&origin=" + orig.lat + "," + orig.lng +
+                            "&destination=" + data.features[i].geometry.coordinates[1] + "," + data.features[i].geometry.coordinates[0]
           directionsDisplay.setMap(map);
+          prompt('xd')
+          //console.log(href.match(/([^\/]*)\/*$/)[1]);
+          window.open(markerLink);
 
+          
           console.log(
             data.features[i].geometry.coordinates[1],
             data.features[i].geometry.coordinates[0]
