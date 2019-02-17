f5f8d62bce128c50fd10d094828790eb64f2c2a4
JackyLiu97 <jackyliu977@gmail.com>
2019-02-16 19:21:58 -0500

    async to fetch the convert the geoJSON to JSON

--- vanilla/main.js
+++ vanilla/main.js
@@ -1,15 +1,16 @@
-var map;
+var map, infoWindow;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10.75,
-    center: { lat: 40.7128, lng: -74.006 }
+    center: { lat: 40.7128, lng: -74.006 } // location of NYC 
   });
 
   // NOTE: This uses cross-domain XHR, and may not work on older browsers.
-  map.data.loadGeoJson("https://s3.amazonaws.com/litter-basket/data.json");
+   map.data.loadGeoJson("https://s3.amazonaws.com/litter-basket/data.json");
+   
 
   infoWindow = new google.maps.InfoWindow();
-
+  
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       function(position) {
@@ -22,7 +23,8 @@ function initMap() {
         infoWindow.setContent("Location found.");
         infoWindow.open(map);
         map.setCenter(pos);
-      },
+        map.setZoom(17);
+        },
       function() {
         handleLocationError(true, infoWindow, map.getCenter());
       }
@@ -41,17 +43,29 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
       : "Error: Your browser doesn't support geolocation."
   );
   infoWindow.open(map);
-
-  map.addListener("center_changed", function() {
-    // 3 seconds after the center of the map has changed, pan back to the
-    // marker.
-    window.setTimeout(function() {
-      map.panTo(marker.getPosition());
-    }, 3000);
-  });
-
-  marker.addListener("click", function() {
-    map.setZoom(25);
-    map.setCenter(marker.getPosition());
-  });
 }
+
+// gets all the coordinates of the trash can GEOJson
+getAllCoordinates = () => {
+  fetch("https://s3.amazonaws.com/litter-basket/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+  })
+  .catch((err) => {
+    console.log("Error failed to get data",err)
+  })
+}
+/*
+let location = () => {
+  console.log(map.data.features.geometry.coordinates[0]);
+  console.log(map.data.features.geometry.coordinates[1]);
+}
+*/
+
+/*
+map.data.addListener('click') =>  {
+  console.log(map.data.features.geometry.coordinates[0]);
+  console.log(map.data.features.geometry.coordinates[1]);
+}
+*/
