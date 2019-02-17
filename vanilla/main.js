da6eead8e6b64280c1c776fed2810c6b0076635e
Daniel Aguilar-Rodriguez <daniel.a-r@outlook.com>
2019-02-16 19:35:08 -0500

    change position of fetch

--- vanilla/main.js
+++ vanilla/main.js
@@ -2,15 +2,30 @@ var map, infoWindow;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10.75,
-    center: { lat: 40.7128, lng: -74.006 } // location of NYC 
+    center: { lat: 40.7128, lng: -74.006 } // location of NYC
   });
 
   // NOTE: This uses cross-domain XHR, and may not work on older browsers.
-   map.data.loadGeoJson("https://s3.amazonaws.com/litter-basket/data.json");
-   
+  map.data.loadGeoJson("https://s3.amazonaws.com/litter-basket/data.json");
+
+  fetch("https://s3.amazonaws.com/litter-basket/data.json")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      // Work with JSON data here
+      for (let i = 0; i < 10; i++)
+        console.log(
+          data.features[i].geometry.coordinates[1],
+          data.features[i].geometry.coordinates[0]
+        );
+    })
+    .catch(err => {
+      console.log("Error failed to get data", err);
+    });
 
   infoWindow = new google.maps.InfoWindow();
-  
+
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       function(position) {
@@ -24,7 +39,7 @@ function initMap() {
         infoWindow.open(map);
         map.setCenter(pos);
         map.setZoom(17);
-        },
+      },
       function() {
         handleLocationError(true, infoWindow, map.getCenter());
       }
@@ -48,14 +63,14 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 // gets all the coordinates of the trash can GEOJson
 getAllCoordinates = () => {
   fetch("https://s3.amazonaws.com/litter-basket/data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data)
-  })
-  .catch((err) => {
-    console.log("Error failed to get data",err)
-  })
-}
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(err => {
+      console.log("Error failed to get data", err);
+    });
+};
 /*
 let location = () => {
   console.log(map.data.features.geometry.coordinates[0]);
